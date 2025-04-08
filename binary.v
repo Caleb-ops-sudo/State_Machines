cfa1f767b16ce29b5be3f61c6c2e51848fc1e919
Caleb Cook <calebcook766@u.boisestate.edu>
2025-04-08 11:44:43 -0600

    Update binary.v

--- binary.v
+++ binary.v
@@ -1,1 +1,48 @@
+// Implement binary state machine
+
+
+module binary (
+
+input w,
+input clk,
+input Reset, 
+output [2:0] State,
+output z
+);
+
+    wire [2:0] Next;
+
+ dff zero(
+       .D(Next[0]),
+       .clk(clk),
+       .Q(State[0]),
+       .Reset(Reset)
+    );
+
+   dff one(
+       .D(Next[1]),
+       .clk(clk),
+       .Q(State[1]),
+       .Reset(Reset)
+    );
+    
+    dff two(
+        .D(Next[2]),
+        .clk(clk),
+        .Q(State[2]),
+        .Reset(Reset)
+        
+       );
+
+    assign z = (State[1] & ~State[0]) | State[2];
+    
+    assign Next[0] = (~State[0] & ~State[1] & ~State[2]) | (~State[0] & ~State[1] & w)| (~State[0] & ~State[2] & w) | (State[0] & ~State[2] & ~w) | (State[1] & State[2] & ~w);
+    assign Next[1] = (~State[0] & State[1] & ~State[2]) |(~State[0] & ~State[2] & w) | (~State[0] & ~State[1] & State[2]);
+    assign Next[2] =(State[0] & ~State[1] & w) | (State[1] & State[2] & w) ;
+
+
+
+
+
+endmodule
 
