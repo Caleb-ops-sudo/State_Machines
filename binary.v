172179d29f3abf3aaffa4b4c65397cc874c3a7a3
Caleb Cook <calebcook766@u.boisestate.edu>
2025-04-08 23:53:57 -0600

    Update binary.v

--- binary.v
+++ binary.v
@@ -11,8 +11,10 @@ output z
 );
 
     wire [2:0] Next;
+   
 
  dff zero(
+       .Default(1'b0),
        .D(Next[0]),
        .clk(clk),
        .Q(State[0]),
@@ -20,6 +22,7 @@ output z
     );
 
    dff one(
+       .Default(1'b0),
        .D(Next[1]),
        .clk(clk),
        .Q(State[1]),
@@ -27,6 +30,7 @@ output z
     );
     
     dff two(
+        .Default(1'b0),
         .D(Next[2]),
         .clk(clk),
         .Q(State[2]),
@@ -34,18 +38,12 @@ output z
         
        );
 
-    assign z = (State[1] & ~State[0]) | State[2];
-    
-    assign Next[0] = (~State[0] & ~State[1] & ~State[2]) | (~State[0] & ~State[1] & w)| (~State[0] & ~State[2] & w) | (State[0] & ~State[2] & ~w) | (State[1] & State[2] & ~w);
-    assign Next[1] = (~State[0] & State[1] & ~State[2]) |(~State[0] & ~State[2] & w) | (~State[0] & ~State[1] & State[2]);
-    assign Next[2] =(State[0] & ~State[1] & w) | (State[1] & State[2] & w) ;
 
-assign z = (State[1] & ~State[0] & ~State[2]) | (~State[1] & State[2] & ~State[0]);
-
-assign Next[0] = (~State[2] & ~State[1] & ~w) | (State[1] & ~State[0] & w) | (~State[2] & State[0] & ~w) | (~State[1] & State[2] & ~w);
-assign Next[1] = (~State[2] & State[0] & ~w) | (~State[2] & State[1] & ~State[0]) | (State[2] & State[0] & w);
-assign Next[2] = (State[1] & ~State[0] & w) | (~State[1] & State[2] & w);
+     assign z = (State[1] & ~State[2] &  ~State[0]) | (State[2] & ~State[0] & ~State[1]);                                     
+     assign Next[0] = (~State[1] & ~State[0] &  ~w) | (State[1] & State[0] & ~w) | ( ~State[2] & ~State[1] & w) | (~State[2] & ~State[0] & w) ;
+     assign Next[1] = (~State[2] & ~State[0] & w) | (State[1] & ~State[0]) | (~State[1] & State[0]) | (~State[2] & ~State[0] & w);
+     assign Next[2] = (State[1] & State[0] & w) | (State[2] & w);
+   
 
 
 endmodule
-
